6df235222e79ef3d7cfef01f9287ff6b580947a2
Kaue Reinbold <ck_reinbold@hotmail.com>
2018-04-16 00:20:45 -0300

    It has started the migration to AspNetCore 2.0

---
+++ Reminders.Infrastructure/Reminders.Context/RemindersContext/RemindersDbContext.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reminders.Context.RemindersContext
+{
+    public class RemindersDbContext : DbContext
+    {
+        public RemindersDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
---
+++ Reminders.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reminders.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
