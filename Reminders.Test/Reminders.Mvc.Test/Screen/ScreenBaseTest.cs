b5e6d3548c49128d572e14706c0c0d331ec96bfe
Kaue Reinbold <ck_reinbold@hotmail.com>
2018-04-18 01:42:47 -0300

    Added unit test.

---
+++ Reminders.Test/Reminders.Mvc.Test/Screen/ScreenBaseTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Reminders.Mvc.Test.Screen
+{
+    [TestClass]
+    public class ScreenBaseTest
+    {
+        public IWebDriver _webDriver;
+        
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _webDriver = new ChromeDriver($"{Environment.CurrentDirectory}/Drivers");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
