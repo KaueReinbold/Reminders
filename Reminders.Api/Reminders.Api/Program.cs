f47e054beeea0fe4678ecfeba98eca37bf7abae0
Kaue Reinbold <ck_reinbold@hotmail.com>
2018-04-22 21:30:13 -0300

    Added reminders api.

---
+++ Reminders.Api/Reminders.Api/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Reminders.Api
+{
+    /// <summary>
+    /// Program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main.
+        /// </summary>
+        /// <param name="args"> args array. </param>
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        /// <summary>
+        /// Build.
+        /// </summary>
+        /// <param name="args"> args array. </param>
+        /// <returns> IWebHost object. </returns>
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
