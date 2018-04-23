e31bf2f240ad134ad5fb8555f95477e7bc5b7011
Kaue Reinbold <ck_reinbold@hotmail.com>
2018-04-23 01:15:38 -0300

    Updated Api.

--- Reminders.Api/Program.cs
+++ Reminders.Api/Program.cs
@@ -1,27 +1,22 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Reminders.Api
 {
-    /// <summary>
-    /// Program class.
-    /// </summary>
     public class Program
     {
-        /// <summary>
-        /// Main.
-        /// </summary>
-        /// <param name="args"> args array. </param>
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
 
-        /// <summary>
-        /// Build.
-        /// </summary>
-        /// <param name="args"> args array. </param>
-        /// <returns> IWebHost object. </returns>
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
---
+++ Reminders.Test/Reminders.Mvc.Test/Api/TestConfigurationApi.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Net.Http;
+
+namespace Reminders.Mvc.Test.Api
+{
+    public class TestConfigurationApi
+    {
+        public IConfigurationRoot _configuration;
+        public ServiceProvider _serviceProvider;
+        public IMapper _mapper;
+        public HttpClient _httpClient;
+
+        public TestConfigurationApi()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            _httpClient = new HttpClient();
+        }
+    }
+}
