0105fd381981cd031d7e8acfde99024e7d8cda35
Kaue Reinbold <ck_reinbold@hotmail.com>
2018-04-23 01:28:56 -0300

    Added reminder controller.

---
+++ Reminders.Api/Controllers/RemindersController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Reminders.Business.Contracts;
+using Reminders.Domain.Models;
+using Reminders.Domain.Extensions;
+
+namespace Reminders.Api.Controllers
+{
+    /// <summary>
+    /// Controller of Reminders.
+    /// </summary>
+    [Route("api/[controller]")]
+    public class RemindersController : Controller
+    {
+        private readonly ILogger<RemindersController> _logger;
+        private readonly IBusinessModelGeneric<ReminderModel> _businessReminderModel;
+
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="businessReminderModel"></param>
+        public RemindersController(ILogger<RemindersController> logger, IBusinessModelGeneric<ReminderModel> businessReminderModel)
+        {
+            _logger = logger;
+            _businessReminderModel = businessReminderModel;
+        }
+
+        /// <summary>
+        /// Get all reminders.
+        /// </summary>
+        /// <returns> Return a json array with all reminders. </returns>
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            var reminders = _businessReminderModel.GetAll();
+
+            return reminders.ToJson();
+        }
+
+        /// <summary>
+        /// Get reminder by id.
+        /// </summary>
+        /// <param name="id"> Int variables that represents an Id of a reminder. </param>
+        /// <returns> Return a json object with a reminder. </returns>
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            var reminder = _businessReminderModel.Find(id);
+
+            return reminder.ToJson();
+        }
+
+        /// <summary>
+        /// Post that insert a reminder.
+        /// </summary>
+        /// <param name="reminder"> Object that have all the attributes of a reminder. </param>
+        /// <returns> Returns the status of process. </returns>
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]ReminderModel reminder)
+        {
+            var reminderCreated = _businessReminderModel.Insert(reminder);
+
+            if (reminderCreated.Id == 0)
+                return BadRequest("Some error has happened!");
+
+            return Ok("Reminder has created!");
+        }
+
+        /// <summary>
+        /// Put that update a reminder.
+        /// </summary>
+        /// <param name="reminder"> Object that have all the attributes of a reminder to update. </param>
+        /// <returns> Returns the status of process. </returns>
+        // PUT api/values
+        [HttpPut]
+        public IActionResult Put([FromBody]ReminderModel reminder)
+        {
+            var reminderEdit = _businessReminderModel.Update(reminder);
+
+            if (!reminderEdit)
+                return BadRequest("Some error has happened!");
+
+            return Ok("Reminder has updated!");
+        }
+
+        /// <summary>
+        /// Delete that remove a reminder.
+        /// </summary>
+        /// <param name="id"> Int variables that represents an Id of a reminder. </param>
+        /// <returns></returns>
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var hasDeleted = _businessReminderModel.Delete(id);
+
+            if (!hasDeleted)
+                return BadRequest("Some error has happened!");
+
+            return Ok("Reminder has deleted!");
+        }
+    }
+}
--- Reminders.Api/Startup.cs
+++ Reminders.Api/Startup.cs
@@ -1,40 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Reminders.Business.BusinessModels;
+using Reminders.Business.Contracts;
+using Reminders.Business.RepositoryEntities;
+using Reminders.Context.RemindersContext;
+using Reminders.Domain.Entities;
+using Reminders.Domain.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+using AutoMapper;
 
 namespace Reminders.Api
 {
+    /// <summary>
+    /// Startup class.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Constructor of the class.
+        /// </summary>
+        /// <param name="configuration"> IConfiguration object. </param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// IConfiguration.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"> IServiceCollection object. </param>
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IRepositoryEntityGeneric<ReminderEntity>, RepositoryReminderEntity>();
+
+            services.AddScoped<IBusinessModelGeneric<ReminderModel>, BusinessReminderModel>();
+
+            services.AddDbContext<RemindersDbContext>(options =>
+                      options.UseSqlServer(Configuration.GetConnectionString("StringConnectionReminders")));
+
+            services.AddAutoMapper();
+
+            services.AddLogging();
+
             services.AddMvc();
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1",
+            //        new Info
+            //        {
+            //            Title = "Reminders",
+            //            Version = "v1",
+            //            Description = "Application to register and list reminders. Each reminder has a Title, Description, Date Limit and Status.",
+            //            Contact = new Swashbuckle.AspNetCore.Swagger.Contact
+            //            {
+            //                Name = "Kaue Reinbold",
+            //                Url = "https://github.com/KaueReinbold"
+            //            }
+            //        });
+
+            //    string caminhoAplicacao =
+            //        PlatformServices.Default.Application.ApplicationBasePath;
+            //    string nomeAplicacao =
+            //        PlatformServices.Default.Application.ApplicationName;
+            //    string caminhoXmlDoc =
+            //        Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+
+            //    c.IncludeXmlComments(caminhoXmlDoc);
+            //});
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"> IApplicationBuilder object. </param>
+        /// <param name="env"> IHostingEnvironment object. </param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
             }
+            app.UseDeveloperExceptionPage();
 
             app.UseMvc();
+
+            //app.UseSwagger();
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json",
+            //        "Reminders");
+            //});
         }
     }
 }
