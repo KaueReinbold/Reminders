e479839f2c0c7a91354acdf5415e38eda6e4fb75
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-02-12 22:38:21 -0300

    Added OnConfiguring method to allow migration to be applied through Reminders.Infrastructure.Data project.

--- src/Reminders.Infrastructure.Data/Contexts/RemindersContext.cs
+++ src/Reminders.Infrastructure.Data/Contexts/RemindersContext.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Reminders.Infrastructure.Data.EntityFramework.Configurations;
+using System;
+using System.IO;
 
 namespace Reminders.Infrastructure.Data.EntityFramework.Contexts
 {
     public class RemindersContext
         : DbContext
     {
+        public RemindersContext() { }
+
         public RemindersContext(DbContextOptions<RemindersContext> options)
             : base(options) { }
 
@@ -14,6 +19,22 @@ namespace Reminders.Infrastructure.Data.EntityFramework.Contexts
             modelBuilder.ApplyConfiguration(new RemindersConfiguration());
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile($"appsettings.{environment}.json")
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            }
         }
     }
 }
