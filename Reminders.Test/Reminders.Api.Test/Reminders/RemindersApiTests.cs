620c55402bbcd967715ac937844be359365d69c8
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-02-13 20:51:00 -0300

    Removed old project reference, just leaving the MVC test to follow Selenium pattern.

--- Reminders.Test/Reminders.Api.Test/Reminders/RemindersApiTests.cs
+++ Reminders.Test/Reminders.Api.Test/Reminders/RemindersApiTests.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Reminders.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using Reminders.Domain.Extensions;
 
 namespace Reminders.Api.Test.Reminders
 {
