f2b92788d915fd93879e6a3c11e2f6a97d8efe1e
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-02-23 00:34:23 -0300

    Added dotnetcore build on Github. #37

--- src/Reminders.Application/Extensions/ApplicationConfiguration.cs
+++ src/Reminders.Application/Extensions/ApplicationConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Reminders.Application.Contracts;
 using Reminders.Application.Mapper.Extensions;
@@ -7,16 +8,16 @@ using Reminders.Infrastructure.CrossCutting;
 
 namespace Reminders.Application.Extensions
 {
+    [ExcludeFromCodeCoverage]
     public static class ApplicationConfiguration
     {
-        public static IServiceCollection RegisterApplicationServices(
-            this IServiceCollection services,
-            IConfiguration configuration) =>
+        public static IServiceCollection RegisterApplicationServices (
+                this IServiceCollection services,
+                IConfiguration configuration) =>
             services
-                .AddSingleton(AutoMapperConfiguration.CreateMapper())
-                .RegisterDataServices(configuration)
-                .AddScoped<IRemindersService, RemindersService>()
-            ;
+            .AddSingleton (AutoMapperConfiguration.CreateMapper ())
+            .RegisterDataServices (configuration)
+            .AddScoped<IRemindersService, RemindersService> ();
 
     }
 }
