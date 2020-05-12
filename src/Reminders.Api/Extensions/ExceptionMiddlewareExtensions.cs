7b1ce135cedf1144488953c9a39c359e5e523c1a
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-05-12 09:22:30 -0300

    Treat FluentValidation errors.

--- src/Reminders.Api/Extensions/ExceptionMiddlewareExtensions.cs
+++ src/Reminders.Api/Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Reminders.Api.Models;
 using Reminders.Application.Validators.Reminders.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace Reminders.Api.Extensions
@@ -23,20 +26,31 @@ namespace Reminders.Api.Extensions
 
                     if (contextFeature != null)
                     {
-                        var statusCode = context.Response.StatusCode;
+                        var statusCode = (int)HttpStatusCode.InternalServerError;
                         var message = "Internal Server Error.";
+                        Dictionary<string, string> properties = null;
 
                         if (contextFeature.Error is RemindersApplicationException remindersApplicationException)
                         {
                             statusCode = (int)remindersApplicationException.ToHttpStatusCode();
                             message = remindersApplicationException.Message;
-                            context.Response.StatusCode = statusCode;
                         }
+                        else if (contextFeature.Error is ValidationException validationException)
+                        {
+                            statusCode = (int)HttpStatusCode.UnprocessableEntity;
+                            message = validationException.Message;
+                            properties = validationException.Errors.ToDictionary(
+                                error => error.PropertyName,
+                                error => error.ErrorMessage);
+                        }
+
+                        context.Response.StatusCode = statusCode;
 
                         await context.Response.WriteAsync(new ErrorDetails()
                         {
                             StatusCode = statusCode,
-                            Message = message
+                            Message = message,
+                            Properties = properties
                         }.ToString());
                     }
                 });
