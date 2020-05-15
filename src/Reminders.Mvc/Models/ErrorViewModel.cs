57d3915327f910036d01f6f5b62c143a561aedea
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-05-15 13:17:52 -0300

    Create Reminders library structure.

--- src/Reminders.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Reminders.Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
