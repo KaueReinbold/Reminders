fa1bce75027c46937737ae5b6294bcb91b62f70f
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-06-16 21:35:41 -0300

    Execute script to create database tables

--- infrastructure/mssql/configure-database.sh
+++ infrastructure/mssql/configure-database.sh
@@ -20,4 +20,7 @@ echo "======= MSSQL SERVER STARTED ========" | tee -a ./config.log
 # Run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i create-user.sql
 
+# Run the setup script to create the tables
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i create-tables.sql
+
 echo "======= MSSQL CONFIG COMPLETE =======" | tee -a ./config.log
