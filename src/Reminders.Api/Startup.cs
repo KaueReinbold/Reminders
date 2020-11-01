b27f3f13e4179132d118f245680d98cc85e66d60
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-11-01 08:59:42 -0300

    Add api projects

---
+++ src/Reminders.Api/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Reminders.Api.Extensions;
+using Reminders.Application.Extensions;
+using Reminders.Infrastructure.CrossCutting;
+
+namespace Reminders.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .RegisterApplicationServices(Configuration)
+                .AddControllers()
+                .AddApplicationValidations(services);
+
+            services
+                .AddSwaggerGen(setup =>
+                    setup.SwaggerDoc("v1", new OpenApiInfo { Title = "Reminders API", Version = "v1" }));
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory logger)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app
+                .UseRemindersExceptionHandler()
+                .UseHttpsRedirection()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints => endpoints.MapControllers())
+                .UseSwagger()
+                .UseSwaggerUI(setup =>
+                    setup.SwaggerEndpoint("/swagger/v1/swagger.json", "Reminders API V1"));
+        }
+    }
+}
