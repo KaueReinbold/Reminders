7fef92d318de71f214ad0f5834fd85aa521571f4
Kaue Reinbold <kaue.reinbold@outlook.com>
2023-12-21 23:28:52 -0300

    feat: update to dotnet 8

--- src/Web/Reminders.Api/Controllers/RemindersController.cs
+++ src/Web/Reminders.Api/Controllers/RemindersController.cs
@@ -1,43 +1,37 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
-using Reminders.Application.Contracts;
-using Reminders.Application.ViewModels;
+﻿namespace Reminders.Api.Controllers;
 
-namespace Reminders.Api.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class RemindersController
+    : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RemindersController
-        : ControllerBase
-    {
-        private readonly IRemindersService remindersService;
+    private readonly IRemindersService remindersService;
 
-        public RemindersController(IRemindersService remindersService) =>
-            this.remindersService = remindersService;
+    public RemindersController(IRemindersService remindersService) =>
+        this.remindersService = remindersService;
 
-        // GET: api/Reminders
-        [HttpGet]
-        public IActionResult Get() =>
-            Ok(remindersService.Get());
+    // GET: api/Reminders
+    [HttpGet]
+    public IActionResult Get() =>
+        Ok(remindersService.Get());
 
-        // GET: api/Reminders/5
-        [HttpGet("{id}", Name = "Get")]
-        public IActionResult Get(Guid id) =>
-            Ok(remindersService.Get(id));
+    // GET: api/Reminders/5
+    [HttpGet("{id}", Name = "Get")]
+    public IActionResult Get(Guid id) =>
+        Ok(remindersService.Get(id));
 
-        // POST: api/Reminders
-        [HttpPost]
-        public ReminderViewModel Post([FromBody] ReminderViewModel reminderViewModel) =>
-            remindersService.Insert(reminderViewModel);
+    // POST: api/Reminders
+    [HttpPost]
+    public ReminderViewModel Post([FromBody] ReminderViewModel reminderViewModel) =>
+        remindersService.Insert(reminderViewModel);
 
-        // PUT: api/Reminders/5
-        [HttpPut("{id}")]
-        public ReminderViewModel Put(Guid id, [FromBody] ReminderViewModel reminderViewModel) =>
-            remindersService.Edit(id, reminderViewModel);
+    // PUT: api/Reminders/5
+    [HttpPut("{id}")]
+    public ReminderViewModel Put(Guid id, [FromBody] ReminderViewModel reminderViewModel) =>
+        remindersService.Edit(id, reminderViewModel);
 
-        // DELETE: api/Reminders/5
-        [HttpDelete("{id}")]
-        public void Delete(Guid id) =>
-            remindersService.Delete(id);
-    }
+    // DELETE: api/Reminders/5
+    [HttpDelete("{id}")]
+    public void Delete(Guid id) =>
+        remindersService.Delete(id);
 }
