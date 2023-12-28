0a5435a589ebdb0eec44e706c28a1ff1646a94d2
Kaue Reinbold <kaue.reinbold@outlook.com>
2023-12-27 23:23:10 -0300

    feat: add stress test configuration

---
+++ src/Services/Reminders.Infrastructure.CrossCutting/Extensions/MachineNameMiddlewareExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Reminders.Infrastructure.CrossCutting.Extensions;
+
+public static class MachineNameMiddlewareExtensions
+{
+    public static IApplicationBuilder UseMachineNameLogging<T>(this IApplicationBuilder app)
+    {
+        app.Use(async (context, next) =>
+        {
+            context
+                .RequestServices
+                .GetRequiredService<ILogger<T>>()
+                .LogInformation($"\t\nMachineName: {Environment.MachineName} \t\nSystem: {Environment.OSVersion.VersionString} \n");
+
+            await next.Invoke();
+        });
+
+        return app;
+    }
+}
