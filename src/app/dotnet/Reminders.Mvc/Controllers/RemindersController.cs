12537266b060329ffd6feec74bab93bc042655a5
Kaue Reinbold <kaue.reinbold@outlook.com>
2024-01-01 17:19:50 -0300

    refactor: simplify namespace for mvc

--- src/app/dotnet/Reminders.Mvc/Controllers/RemindersController.cs
+++ src/app/dotnet/Reminders.Mvc/Controllers/RemindersController.cs
@@ -1,139 +1,138 @@
-﻿namespace Reminders.Mvc.Controllers
+﻿namespace Reminders.Mvc.Controllers;
+
+// FIXME: Do not redirect user to a different page when an error occurred. Show a friendly message.
+public class RemindersController : Controller
 {
-    // FIXME: Do not redirect user to a different page when an error occurred. Show a friendly message.
-    public class RemindersController : Controller
+    private ILogger<RemindersController> logger;
+    private IRemindersService remindersService;
+
+    public RemindersController(
+        ILogger<RemindersController> logger,
+        IRemindersService remindersService)
     {
-        private ILogger<RemindersController> logger;
-        private IRemindersService remindersService;
+        this.logger = logger;
+        this.remindersService = remindersService;
+    }
 
-        public RemindersController(
-            ILogger<RemindersController> logger,
-            IRemindersService remindersService)
+    // GET: Reminders
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        try
         {
-            this.logger = logger;
-            this.remindersService = remindersService;
+            return View(await remindersService.GetRemindersAsync(cancellationToken));
+        }
+        catch (ApiException ex)
+        {
+            ConvertApiExceptionToModalStateErrors(ex);
+
+            return View(new List<ReminderViewModel>());
+        }
+    }
+
+    // GET: Reminders/Details/5
+    public async Task<IActionResult> Details(Guid id, CancellationToken cancellationToken)
+        => await GetReminder(id, cancellationToken);
+
+    // GET: Reminders/Create
+    public IActionResult Create() =>
+        View();
+
+    // POST: Reminders/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([FromForm] ReminderViewModel reminder, CancellationToken cancellationToken)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                await remindersService.AddReminderAsync(reminder, cancellationToken);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(reminder);
+        }
+        catch (ApiException ex)
+        {
+            ConvertApiExceptionToModalStateErrors(ex);
+
+            return View();
+        }
+    }
+
+    // GET: Reminders/Edit/5
+    public async Task<IActionResult> Edit(Guid id, CancellationToken cancellationToken)
+        => await GetReminder(id, cancellationToken);
+
+    // POST: Reminders/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Guid id, [FromForm] ReminderViewModel reminder, CancellationToken cancellationToken)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                await remindersService.EditReminderAsync(id, reminder, cancellationToken);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(reminder);
+        }
+        catch (ApiException ex)
+        {
+            ConvertApiExceptionToModalStateErrors(ex);
+
+            return View();
         }
 
-        // GET: Reminders
-        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    }
+
+    // GET: Reminders/Delete/5
+    public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+        => await GetReminder(id, cancellationToken);
+
+    // POST: Reminders/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(Guid id, [FromForm] ReminderViewModel reminder, CancellationToken cancellationToken)
+    {
+        await remindersService.DeleteReminderAsync(id, cancellationToken);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    private void ConvertApiExceptionToModalStateErrors(ApiException ex)
+    {
+        if (ex.ErrorResponse?.Errors is not null)
         {
-            try
+            foreach (var (key, errors) in ex.ErrorResponse.Errors)
             {
-                return View(await remindersService.GetRemindersAsync(cancellationToken));
-            }
-            catch (ApiException ex)
-            {
-                ConvertApiExceptionToModalStateErrors(ex);
-
-                return View(new List<ReminderViewModel>());
-            }
-        }
-
-        // GET: Reminders/Details/5
-        public async Task<IActionResult> Details(Guid id, CancellationToken cancellationToken)
-            => await GetReminder(id, cancellationToken);
-
-        // GET: Reminders/Create
-        public IActionResult Create() =>
-            View();
-
-        // POST: Reminders/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([FromForm] ReminderViewModel reminder, CancellationToken cancellationToken)
-        {
-            try
-            {
-                if (ModelState.IsValid)
+                foreach (var error in errors)
                 {
-                    await remindersService.AddReminderAsync(reminder, cancellationToken);
-
-                    return RedirectToAction(nameof(Index));
-                }
-
-                return View(reminder);
-            }
-            catch (ApiException ex)
-            {
-                ConvertApiExceptionToModalStateErrors(ex);
-
-                return View();
-            }
-        }
-
-        // GET: Reminders/Edit/5
-        public async Task<IActionResult> Edit(Guid id, CancellationToken cancellationToken)
-            => await GetReminder(id, cancellationToken);
-
-        // POST: Reminders/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [FromForm] ReminderViewModel reminder, CancellationToken cancellationToken)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    await remindersService.EditReminderAsync(id, reminder, cancellationToken);
-
-                    return RedirectToAction(nameof(Index));
-                }
-
-                return View(reminder);
-            }
-            catch (ApiException ex)
-            {
-                ConvertApiExceptionToModalStateErrors(ex);
-
-                return View();
-            }
-
-        }
-
-        // GET: Reminders/Delete/5
-        public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
-            => await GetReminder(id, cancellationToken);
-
-        // POST: Reminders/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Delete(Guid id, [FromForm] ReminderViewModel reminder, CancellationToken cancellationToken)
-        {
-            await remindersService.DeleteReminderAsync(id, cancellationToken);
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        private void ConvertApiExceptionToModalStateErrors(ApiException ex)
-        {
-            if (ex.ErrorResponse?.Errors is not null)
-            {
-                foreach (var (key, errors) in ex.ErrorResponse.Errors)
-                {
-                    foreach (var error in errors)
-                    {
-                        ModelState.AddModelError(key, error);
-                    }
+                    ModelState.AddModelError(key, error);
                 }
             }
-            else
-            {
-                ModelState.AddModelError("Error", ApiException.TROUBLE_CONNECTING_SERVERS);
-            }
         }
-
-        private async Task<IActionResult> GetReminder(Guid id, CancellationToken cancellationToken)
+        else
         {
-            try
-            {
-                return View(await remindersService.GetReminderAsync(id, cancellationToken));
-            }
-            catch (ApiException ex)
-            {
-                ConvertApiExceptionToModalStateErrors(ex);
+            ModelState.AddModelError("Error", ApiException.TROUBLE_CONNECTING_SERVERS);
+        }
+    }
 
-                return View();
-            }
+    private async Task<IActionResult> GetReminder(Guid id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return View(await remindersService.GetReminderAsync(id, cancellationToken));
+        }
+        catch (ApiException ex)
+        {
+            ConvertApiExceptionToModalStateErrors(ex);
+
+            return View();
         }
     }
 }
