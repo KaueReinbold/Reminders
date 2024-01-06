1e8e68f9fa6b370d3c0f3594c6cbaa493e175ddd
Kaue Reinbold <kaue.reinbold@outlook.com>
2024-01-06 09:21:02 -0300

    feat(react): add reminders deletion
    Refs: #93

--- src/app/reactjs/reminders-app/src/app/reminder/[id]/page.tsx
+++ src/app/reactjs/reminders-app/src/app/reminder/[id]/page.tsx
@@ -2,15 +2,13 @@
 
 import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
-import { TextField, Button, Container, Grid, Checkbox, FormControlLabel } from '@material-ui/core';
+import { TextField, Button, Container, Grid, Checkbox, FormControlLabel, Modal, Box, Typography } from '@material-ui/core';
 
-import { Reminder } from '@/components/RemindersList';
+import { Reminder } from '@/app/components/RemindersList';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export function formatDate(dateString: string) {
-  console.log("🚀 ~ file: page.tsx:12 ~ formatDate ~ dateString:", dateString);
-
   const date = new Date(dateString);
   const year = date.getFullYear();
   const month = ('0' + (date.getMonth() + 1)).slice(-2); // Months are 0-indexed in JavaScript
@@ -18,11 +16,31 @@ export function formatDate(dateString: string) {
   return `${year}-${month}-${day}`;
 }
 
+const style = {
+  modalContent: {
+    position: 'absolute' as 'absolute',
+    top: '20%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    borderRadius: 20,
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+  },
+  modalElement: {
+    marginBottom: '2rem'
+  }
+};
+
+
 export default function Edit() {
   const { id } = useParams<{ id: string }>()
   const router = useRouter();
-  
+
   const [reminder, setReminder] = useState<Reminder>({ id, title: '', description: '', limitDate: '', isDone: false });
+  const [openDelete, setOpenDelete] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -35,11 +53,32 @@ export default function Edit() {
       },
       body,
     });
-    const data = await response.json();
 
     if (response.ok) {
       handleBack();
     }
+  };
+
+  const handleDelete = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetch(`${API_BASE_URL}/api/reminders/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      handleBack();
+    }
+  };
+
+  const handleOpenDelete = () => {
+    setOpenDelete(true);
+  };
+  const handleCloseDelete = () => {
+    setOpenDelete(false);
   };
 
   const handleBack = () => {
@@ -127,12 +166,40 @@ export default function Edit() {
             <Button type="submit" variant="contained" color="primary">
               Edit
             </Button>
+            <Button variant="contained" color="secondary" onClick={handleOpenDelete}>
+              Delete
+            </Button>
             <Button variant="contained" onClick={handleBack}>
               Back
             </Button>
           </Grid>
         </Grid>
       </form>
+
+      <Modal
+        open={openDelete}
+        onClose={handleCloseDelete}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style.modalContent }}>
+          <Box sx={{ ...style.modalElement }}>
+            <Typography variant="h4">
+              Delete reminder
+            </Typography>
+          </Box>
+
+          <Box sx={{ ...style.modalElement }}>
+            <Typography variant="body1">
+              Are you sure you want to delete this reminder?
+            </Typography>
+          </Box>
+
+          <Button variant="contained" color="secondary" onClick={handleDelete}>Delete</Button>
+
+          <Button variant="contained" onClick={handleCloseDelete}>Close</Button>
+        </Box>
+      </Modal>
     </Container>
   );
 }
