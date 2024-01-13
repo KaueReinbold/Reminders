c098a2477876204271b0573704309765002e768d
Kaue Reinbold <kaue.reinbold@outlook.com>
2024-01-13 16:11:09 -0300

    test(react): add useRemindersQueryClient tests
    Refs: #123

---
+++ src/app/reactjs/reminders-app/src/app/hooks/useRemindersQueryClient/index.test.tsx
@@ -0,0 +1,58 @@
+import { act, render, screen } from '@testing-library/react';
+import { QueryClient, useQueryClient } from 'react-query';
+import {
+  ReminderQueryProvider,
+  useRemindersQueryClient,
+  useMutation,
+  useQuery,
+} from '.';
+import { ReactNode } from 'react';
+
+jest.mock('react-query', () => ({
+  ...jest.requireActual('react-query'),
+  useQueryClient: jest.fn(),
+  QueryClientProvider: ({ children }: { children: any }) => children,
+  QueryClient: jest.fn(),
+}));
+
+const TestComponent = () => {
+  useRemindersQueryClient();
+
+  return <></>;
+};
+
+describe('ReminderQueryProvider', () => {
+  async function renderProvider(children: ReactNode) {
+    await act(async () => {
+      render(<ReminderQueryProvider>{children}</ReminderQueryProvider>);
+    });
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('render children with QueryClientProvider', async () => {
+    await renderProvider(<div>Test Child</div>);
+
+    const childElement = screen.getByText('Test Child');
+    expect(childElement).toBeInTheDocument();
+  });
+
+  it('use QueryClientProvider with a new QueryClient', async () => {
+    await renderProvider(<div>Test Child</div>);
+
+    expect(QueryClient).toHaveBeenCalledTimes(1);
+  });
+
+  it('use useQueryClient hook', () => {
+    render(<TestComponent />);
+
+    expect(useQueryClient).toHaveBeenCalledTimes(1);
+  });
+
+  it('should method be defined', () => {
+    expect(useQuery).toBeDefined();
+    expect(useMutation).toBeDefined();
+  });
+});
