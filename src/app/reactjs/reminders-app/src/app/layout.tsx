4eabd822a2867c0978b893324784cbfa20c1d972
Kaue Reinbold <kaue.reinbold@outlook.com>
2025-06-29 15:33:16 -0300

    feat: enhance reminder form and context handling with default values and improved layout

--- src/app/reactjs/reminders-app/src/app/layout.tsx
+++ src/app/reactjs/reminders-app/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 
-import type { Metadata } from 'next'
+import type { Metadata } from 'next/types'
 
 import { ReminderQueryProvider, RemindersContextProvider } from './hooks'
 
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <ReminderQueryProvider>
           <RemindersContextProvider>
             {children}
